69a456145a64840d619ec3e24c222cff90ef2106
Reena0504 <147928901+Reena0504@users.noreply.github.com>
2023-10-18 12:05:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,122 @@
-# Python program to find the factorial of a number provided by the user.
+print("-----Program for Student Information-----")
 
-# change the value for a different result
-num = 7
+D = dict()
 
-# To take input from the user
-#num = int(input("Enter a number: "))
+n = int(input('How many student record you want to store?? '))
 
-factorial = 1
+# Add student information
+# to the dictionary
+for i in range(0, n):
+  x, y = input(
+      "Enter the complete name (First and last name) of student: ").split()
+  z = input("Enter contact number: ")
+  m = input('Enter Marks: ')
+  D[x, y] = (z, m)
 
-# check if the number is negative, positive or zero
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+
+# define a function for shorting
+# names based on first name
+def sort():
+  ls = list()
+  # fetch key and value using
+  # items() method
+  for sname, details in D.items():
+
+    # store key parts as an tuple
+    tup = (sname[0], sname[1])
+
+    # add tuple to the list
+    ls.append(tup)
+
+  # sort the final list of tuples
+  ls = sorted(ls)
+  for i in ls:
+
+    # print first name and second name
+    print(i[0], i[1])
+  return
+
+
+# define a function for
+# finding the minimum marks
+# in stored data
+def minmarks():
+  ls = list()
+  # fetch key and value using
+  # items() methods
+  for sname, details in D.items():
+    # add details second element
+    # (marks) to the list
+    ls.append(details[1])
+
+  # sort the list elements
+  ls = sorted(ls)
+  print("Minimum marks: ", min(ls))
+
+  return
+
+
+# define a function for searching
+# student contact number
+def searchdetail(fname):
+  ls = list()
+
+  for sname, details in D.items():
+
+    tup = (sname, details)
+    ls.append(tup)
+
+  for i in ls:
+    if i[0][0] == fname:
+      print(i[1][0])
+  return
+
+
+# define a function for
+# asking the options
+def option():
+
+  choice = int(
+      input('Enter the operation detail: \n \
+    1: Sorting using first name \n \
+    2: Finding Minimum marks \n \
+    3: Search contact number using first name: \n \
+    4: Exit\n \
+    Option: '))
+
+  if choice == 1:
+    # function call
+    sort()
+    print('Want to perform some other operation??? Y or N: ')
+    inp = input()
+    if inp == 'Y':
+      option()
+
+    # exit function call
+    exit()
+
+  elif choice == 2:
+    minmarks()
+    print('Want to perform some other operation??? Y or N: ')
+
+    inp = input()
+    if inp == 'Y':
+      option()
+    exit()
+
+  elif choice == 3:
+    first = input('Enter first name of student: ')
+    searchdetail(first)
+
+    print('Want to perform some other operation??? Y or N: ')
+    inp = input()
+    if inp == 'Y':
+      option()
+
+    exit()
+  else:
+    print('Thanks for executing me!!!!')
+    exit()
+
+
+option()
